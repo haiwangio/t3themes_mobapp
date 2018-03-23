deedb07550bbab8f5733e824d6455a57ffa9f296
Salvatore Eckel <salvaracer@gmx.de>
2018-03-23 01:14:18 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,9 +16,12 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.18-8.99.99',
+            'typo3' => '7.6.18-9.99.99',
+            't3cms' => '2.0.1-0.0.0',
         ],
         'conflicts' => [],
-        'suggests' => [],
+        'suggests' => [
+            't3content_mobapp' => '2.2.0-0.0.0',
+        ],
     ],
 ];

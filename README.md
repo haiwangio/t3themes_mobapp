ea908b9dd3ad2e86ebc0b50d8a0afe30c4e07740
Salvatore Eckel <salvaracer@gmx.de>
2018-03-27 23:44:26 +0200

    [VERSION] 1.1.0

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a ready to use template for TYPO3. Together with EXT:t3cms you have an e
 ### General Information ###
 
 * t3themes_mobapp
-* v1.0.0
+* v1.1.0
 
 ### How do I get set up? ###
 
--- ext_emconf.php
+++ ext_emconf.php
@@ -13,15 +13,15 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.18-9.99.99',
-            't3cms' => '2.0.1-0.0.0',
+            't3cms' => '2.0.1'
         ],
         'conflicts' => [],
         'suggests' => [
-            't3content_mobapp' => '2.2.0-0.0.0',
+            't3content_mobapp' => '2.2.0'
         ],
     ],
 ];
